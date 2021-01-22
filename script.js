92cab996bc0f87bb51691bdd3d7048f8c8004e6b
Jadyn Calhoun <jaycal2013@gmail.com>
2021-01-22 15:26:03 -0700

    1/22, working on css, unfinished grid, set a couple functions up but didnt finish, html should be finished

--- script.js
+++ script.js
@@ -17,14 +17,32 @@ class Passenger{
         this.extras = extras;
     }
 }
-//next 20 lines copied from other project, still unedited
+
+
+//next ~25 lines copied from other project, still unedited
+
+let passengerList = [];
+let idCount = 1;
+
 function addPassenger(){
+        ////checks that everything != "", that dates are in correct format, things can = "" if not required
+        ///can they drink if date of birth before today 21 years ago then yes they can else no, print along with the rest ofthe passenger information
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
+    let dob = document.getElementById("dob").value;
+    let pointA = document.getElementById("pointA").value;
+    let pointB = document.getElementById("pointB").value;
+    let leavingDate = document.getElementById("leavingDate").value;
+    let returningDate = document.getElementById("returningDate").value;
+    let bags = document.getElementById("bags").value;
+    let meal = document.getElementById("meal").value;
+    let extras = document.getElementById("extras").value;
+
     if(firstName != "" && lastName != ""){
-        let user = new User(firstName, lastName, idCount);
+        let passenger = new Passenger(firstName, lastName, idCount);
         idCount++;
-        userList.push(user);
+        passengerList.push(passenger);
+        ///to reset ..
         document.getElementById("firstName").value = "";
         document.getElementById("lastName").value = "";
     }
@@ -37,3 +55,17 @@ function print(){
         space.innerHTML += `<div><span>${userList[i].ID}</span> ${userList[i].firstName} ${userList[i].lastName}</div>`
     }
 }
+
+function check21(){
+    //needs to run when people submit
+}
+
+function extraCosts(){
+    //needs to run when submitted
+}
+
+function tripTime(){
+    //needs to run when submitted 
+}
+
+let pass1 = new Passenger("Jadyn", "Calhoun", 2384798);
