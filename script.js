4edddcf398aa34ab37c644b13135e38fb35df585
Jadyn Calhoun <jaycal2013@gmail.com>
2021-01-28 15:22:41 -0700

    working on creating full name array and search function

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-///can they drink if date of birth before today 21 years ago then yes they can else no, print along with the rest ofthe passenger information
-
-//print out function should do first and last name as well as id 
-
-//////need to fix age and figure out how to do date
 
 /////////////////////////////////////////////////////////////////////////////
 //remember to set outmeal equal to mealArr and same thinf with extras and such
 class Passenger {
-    constructor(firstName, lastName, idCount, dob, pointA, pointB, leavingDate, returningDate, bags, meal, extras, age, finalCost) {
+    constructor(firstName, lastName, idCount, dob, pointA, pointB, leavingDate, returningDate, bags, meal, extras, age, finalCost, tripDuration, check21) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.idCount = idCount;
@@ -21,6 +16,7 @@ class Passenger {
         this.extras = extras;
         this.age = age;
         this.finalCost = finalCost;
+        this.tripDuration = tripDuration;
     }
     calculateAge() {
         let birthday = new Date(this.dob);
@@ -32,11 +28,21 @@ class Passenger {
     }
     calculateFinalCost() {
         let finalCost = calculateExtrasCost() + calculateBagsCost() + 300;
-        console.log(finalCost);
-        console.log(calculateExtrasCost());
-        console.log(calculateBagsCost());
-        console.log(finalCost);
         this.finalCost = finalCost;
+    }
+    calculateTripDuration(){
+        let leaving = new Date(this.leavingDate);
+        let returning = new Date(this.returningDate);
+        let tripDuration = returning.getTime() - leaving.getTime();
+        tripDuration = tripDuration / (1000 * 60 * 60 * 24);
+        this.tripDuration = tripDuration;
+    }
+    check21(){
+        if(this.age >= 21){
+            this.check21 = "Can drink";
+        }else{
+            this.check21 = "Cannot drink";
+        }
     }
 }
 
@@ -59,12 +65,22 @@ function addPassenger() {
     let bags = document.getElementById("bags").value;
     let extras = findExtras().join(", ");
     let meal = findMeals().join(", ");
+    document.getElementById("extras1").checked = false;
+    document.getElementById("extras2").checked = false;
+    document.getElementById("extras3").checked = false;
+    document.getElementById("extras4").checked = false;
+    document.getElementById("meal1").checked = false;
+    document.getElementById("meal2").checked = false;
+    document.getElementById("meal3").checked = false;
+    
     // let finalCost = calculateFinalCost();
     if (firstName != "" && lastName != "" && dob != "" && pointA != "" && pointB != "" && leavingDate != "" && returningDate != "") {
         let passenger = new Passenger(firstName, lastName, idCount, dob, pointA, pointB, leavingDate, returningDate, bags, meal, extras);
         idCount++;
         passenger.calculateAge();
         passenger.calculateFinalCost();
+        passenger.calculateTripDuration();
+        passenger.check21();
         passengerList.push(passenger);
         //to reset
         document.getElementById("firstName").value = "";
@@ -78,6 +94,7 @@ function addPassenger() {
         document.getElementsByName("extras").value = "";
         document.getElementsByName("meal").value = "";
         // document.getElementById("age").value = "";
+
     } //else{
     //     alert("enter all required information");
     // }
@@ -96,45 +113,46 @@ function addPassenger() {
 function findExtras() {
     let extrasArr = [];
     if (document.getElementById("extras1").checked) {
-        console.log(`legroom is checked`);
         extrasArr.push(document.getElementById("extras1").value);
     }
     if (document.getElementById("extras2").checked) {
-        console.log(`window is checked`);
         extrasArr.push(document.getElementById("extras2").value);
     }
     if (document.getElementById("extras3").checked) {
-        console.log(`headphones is checked`);
         extrasArr.push(document.getElementById("extras3").value);
     }
     if (document.getElementById("extras4").checked) {
-        console.log(`more food is checked`);
         extrasArr.push(document.getElementById("extras4").value);
     }
-    console.log(extrasArr);
     return extrasArr;
 }
 
 function findMeals() {
     let mealArr = [];
     if (document.getElementById("meal1").checked) {
-        console.log(`chicken is selected`);
         mealArr.push(document.getElementById("meal1").value);
     }
     if (document.getElementById("meal2").checked) {
-        console.log(`fish is selected`);
         mealArr.push(document.getElementById("meal2").value);
     }
     if (document.getElementById("meal3").checked) {
-        console.log(`veggie is selected`);
         mealArr.push(document.getElementById("meal3").value);
     }
     return mealArr;
 }
 
+function findFullName(){
+    let fullName = []
+    let firstName = firstName.value;
+    let lastName = lastName.value; 
+    fullName.push(firstName);
+    fullName.push(lastName);
+    console.log(fullName);
+    return fullName;
+}
+
 function calculateBagsCost() {
     let bagsCost = 20 * bags.value;
-    console.log(bagsCost);
     return bagsCost;
 }
 
@@ -143,16 +161,22 @@ function calculateExtrasCost() {
     let extrasCost = findExtras().length * 10;
     return extrasCost;
 }
-// console.log(calculateExtrasCost());
 
-// function calculateFinalCost() {
-//     let finalCost = calculateExtrasCost() + calculateBagsCost() + 300;
-//     console.log(finalCost);
-//     console.log(calculateExtrasCost());
-//     console.log(calculateBagsCost());
-//     return finalCost;
-// }
-// calculateFinalCost();
+
+
+function search() {
+    //ok all items in an array will have their id and each button will have a corresponding number, then when they select their name it will 
+    //depending on which text you choose, it will add a # to a variable and then that variable will be used to search through the array to find your information. it'll then print out into each spot
+    ///always searching first last name
+    let fullNameArr = findFullName();
+    console.log(fullNameArr);
+    // let inputName = document.getElementById("search").value;
+    // let nameArr = name.split("");
+    // console.log(nameArr);
+    // console.log(nameArr.length);
+}
+
+
 
 function print() {
     let space = document.getElementById("printSpace");
@@ -177,22 +201,8 @@ function print() {
     // }
 }
 
-function search() {
-    //ok all items in an array will have their id and each button will have a corresponding number, then when they select their name it will 
-    //depending on which text you choose, it will add a # to a variable and then that variable will be used to search through the array to find your information. it'll then print out into each spot
 
 
-}
-
-function check21() {
-    //needs to run when people submit
-
-}
-
-
-function tripTime() {
-    //needs to run when submitted 
-}
 
 ////notes for self for next similar project
 //in html, for a checkbox like "extras", i shouldve made the names the same, then i shouldve on line 40 searched instead by names. then made each id different so that i could check which ids were checked bcs easier
